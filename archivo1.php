3312ebd74a366d3068b3f781c2ceb9f6f659bfb0
Dominic <dobioso519@g.educaand.es>
2024-10-21 12:28:46 +0200

    Resolución de conflicto: combinación de cambios 2 y 3

--- archivo1.php
+++ archivo1.php
@@ -1,3 +1,8 @@
 hola  2  nacho.
 que tal?
 como estas?
+<<<<<<< HEAD
+(contenido del "cambio 2.0" del coordinador)
+=======
+(contenido del "cambio 3" del colaborador)
+>>>>>>> origin/archivo_conflictos
